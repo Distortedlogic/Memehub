5eefcc174ff563f9865007d8bc522a66980773c7
distortedlogic <54854447+Distortedlogic@users.noreply.github.com>
2021-01-30 08:39:38 -0500

    save

--- swarm-reset.sh
+++
@@ -1,8 +0,0 @@
-docker swarm leave --force
-docker volume rm memehub_sitedata
-docker swarm init
-docker network create --driver overlay web
-cd ../Memehub-Traefik
-docker stack deploy -c docker-stack.yml rev-proxy
-cd ../Memehub
-sh quick-deploy.sh
